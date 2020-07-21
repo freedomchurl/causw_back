e964720f435234a267b3c5931120cb6ec90a3c74
freedomchurl <freedomchurl@naver.com>
2020-07-21 23:57:07 +0900

    개발자 계정 동아리 가입 불가 추가

--- routes/group/signup.js
+++ routes/group/signup.js
@@ -10,34 +10,41 @@ var current_groupId;
 
 /* GET users listing. */
 router.post('/', function (req, res, next) {
-  token = req.headers.authorization;
+	token = req.headers.authorization;
 	decoded = null;
 	try{
-	decoded = jwt.verify(token,private_key)
+		decoded = jwt.verify(token,private_key)
 	}catch(e){
 		res.json({tokenValid:false,payload:null});
 	}
 
 	if(decoded){
-    Promise.all([Group.findOne({
-      attributes: ['id'],
-      where: { name: req.body.group_name },
-    }), User.findOne({
-      attributes: ['id'],
-      where: { login_id: req.body.login_id },
-    })])
-      .then((values) => {
-        UserGroup.create(
-          {userId:values[1].id, groupId:values[0].id},
-        )
-        console.log('sign up success');
-        res.send({tokenValid:true,payload:null});
-      })
-      .catch((err) => {
-        console.error(err);
-        next(err);
-      });
-    }
+		if(decoded.auth == 0)
+		{
+			res.send({tokenValid:true,payload:-1});
+		}
+		else
+		{
+			Promise.all([Group.findOne({
+				attributes: ['id'],
+				where: { name: req.body.group_name },
+			}), User.findOne({
+				attributes: ['id'],
+				where: { login_id: req.body.login_id },
+			})])
+				.then((values) => {
+					UserGroup.create(
+						{userId:values[1].id, groupId:values[0].id},
+					)
+					console.log('sign up success');
+					res.send({tokenValid:true,payload:null});
+				})
+				.catch((err) => {
+					console.error(err);
+					next(err);
+				});
+		}
+	}
 });
 
 module.exports = router;
